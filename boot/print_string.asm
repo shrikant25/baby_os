b0f4cde3be1f063c7e6052f576e7846bd3bc7a8c
shrikant25 <shrikantwankhede24@gmail.com>
2025-01-05 21:40:41 +0530

    added comments :

--- boot/print_string.asm
+++ boot/print_string.asm
@@ -1,33 +1,33 @@
 print_string:
-    pusha
+    pusha   ; stack current values
 
 
-start:
-    mov al, [bx] 
-    cmp al, 0 
-    je done
+start:            ; assuming that starting address of string is keep in bx
+    mov al, [bx]  ; register bx is holding a address, move value present at that address to al
+    cmp al, 0    ; check if the value is end of line i.e 0
+    je done     ; if value is 0 then we are done with the string
 
-    
+    ; display the character
+    ; invoke interrupt 0x10 set ah register 0x0e to trigger teletype output function
     mov ah, 0x0e
     int 0x10 
 
-   
-    add bx, 1
-    jmp start
+    add bx, 1  ; increment the address
+    jmp start  ; go back to start label
 
 done:
-    popa
-    ret
+    popa     ; pop back the values that were stacked before calling the function
+    ret       ; return
 
 
 
 print_nl:
-    pusha
+    pusha        ; stack current values to stack
     
-    mov ah, 0x0e
-    mov al, 0x0a 
-    int 0x10
-    mov al, 0x0d 
+    mov ah, 0x0e    
+    mov al, 0x0a ; move value 10 i.e newline char in al
+    int 0x10     ; generate interrupt
+    mov al, 0x0d ; move to start of line, sometimes after doing new line it may stay in previous lines last horizontal position
     int 0x10
     
     popa
--- boot/print_string_pm.asm
+++ boot/print_string_pm.asm
@@ -1,25 +1,25 @@
 [bits 32]
 
-VIDEO_MEMORY equ 0xb8000
-WHITE_ON_BLACK equ 0x0f
-
+VIDEO_MEMORY equ 0xb8000   ; memory used in x86 systems for video memory output
+WHITE_ON_BLACK equ 0x0f    ; lower 4 bits represent foreground color, 1111 = white
+                           ; upper 4 bites represent background color 0000 = black
 print_string_pm:
-	pusha
-	mov edx, VIDEO_MEMORY
+	pusha           
+	mov edx, VIDEO_MEMORY   ; point edx to video memory
 
 print_string_pm_loop:
-	mov al, [ebx]
-	mov ah, WHITE_ON_BLACK
+	mov al, [ebx]             ; move first char in al
+	mov ah, WHITE_ON_BLACK    ; move color code in ah
 
-	cmp al, 0
-	je print_string_pm_done
+	cmp al, 0                ; check if end of string
+	je print_string_pm_done   ; if it is end then done
 
-	mov [edx], ax
+	mov [edx], ax            ; store value at address pointed by edx
 	
-	add ebx, 1
-	add edx, 2
+	add edx, 2              ; increment edx to next place, next char if any will be stored at new place
+	add ebx, 1             ; increment to next char of input string
 
-	jmp print_string_pm_loop
+	jmp print_string_pm_loop   ; continue with loop
 
 print_string_pm_done:
 	popa 
